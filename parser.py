134aa791fd777aacaaeb354a2adf9bd5d702f91f
kalasoo <ym.kalasoo@gmail.com>
2013-04-23 14:04:16 +0800

    add four args to parser

--- parser.py
+++ parser.py
@@ -1,1 +1,10 @@
+import argparse
 
+parser = argparse.ArgumentParser()
+parser.add_argument('-t', '--type', choices = ('nep2p', 'bt', 'libswift'), help='specify the type of p2p system')
+parser.add_argument('-cf', '--configfile', type=argparse.FileType('r'), help='specify the configuration file')
+parser.add_argument('-d', '--debug', help='enable debug mode', action='store_true')
+parser.add_argument('-l', '--log', help='enable log mode', action='store_true')
+args = parser.parse_args()
+
+print args
