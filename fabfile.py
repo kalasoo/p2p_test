547ce773f79ec496b86772a91d7d038486e0cc6b
kalasoo <ym.kalasoo@gmail.com>
2013-04-28 21:41:26 +0800

    sender, peers assignment

--- fabfile.py
+++ fabfile.py
@@ -3,7 +3,8 @@ from json import loads
 from config import *
 
 def p2p_test(t = 'nep2p', cf = 'config.json', d = False, l = False):
-	#check input
+	# check input
+	print 'Check input ...'
 	if t not in P2P_SYS:
 		print 'Wrong P2P system type'
 		return
@@ -16,16 +17,30 @@ def p2p_test(t = 'nep2p', cf = 'config.json', d = False, l = False):
 
 	d = d if bool(d) else False;
 	l = l if bool(l) else False;
-
-	# print t, cf, d, l 
-	# print config
+	print 'Valid input\n'
 
 	# parse configuration
+	print 'Check configuration file...'
 	if not check_config(config):
 		print 'Configuration file is invalid'
 		return
 	else:
-		print 'Configuration file is valid'
+		print 'Configuration file is valid\n'
+
+	# setup roles
+	print 'Setup roles...'
+	# sender & peers
+	sender = config['nodes'].pop(config['sender'])
+	peers = [v for v in config['nodes'].values()][:config['num_peer']]
+	# roles
+	env.roledef = {
+		'sender': sender[0],
+		'peers': [p[0] for p in peers]
+	}
+	print env.roledef
+	print 'Roles are deployed\n'
+
+
 
 def check_config(config):
 	if config['file_size'] not in FILE_SIZE:
@@ -40,7 +55,9 @@ def check_config(config):
 	if len(config['nodes']) <= 1:
 		print 'nodes'
 		return False
-	if config['sender'] != None and config['sender'] not in config['nodes']:
+	if config['sender'] == None:
+		config['sender'] = DEFAULT_SENDER
+	elif config['sender'] not in config['nodes'].keys():
 		print 'sender'
-		return False
+		return False 
 	return True
