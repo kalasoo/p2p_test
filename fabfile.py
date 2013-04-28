a09c880e10a402534f0676d14e8f9d4cd22b2cad
kalasoo <ym.kalasoo@gmail.com>
2013-04-28 15:04:21 +0800

    input check

--- fabfile.py
+++ fabfile.py
@@ -1,3 +1,20 @@
-from fabic.api import *
+from fabric.api import *
+from json import loads
 
+def p2p_test(t = 'nep2p', cf = 'config.json', d = False, l = False):
+	#check input
+	if t not in ('nep2p', 'bt', 'libswift'):
+		print 'Wrong P2P system type'
+		return
+	try:
+		f = open(cf, 'r')
+		config = loads(f.read())
+	except:
+		print 'Wrong configuration file'
+		return
 
+	d = d if bool(d) else False;
+	l = l if bool(l) else False;
+
+	print t, cf, d, l 
+	print config
