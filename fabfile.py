d8c13e7ec4c80910381089d4a9c2f25fdc99709f
kalasoo <ym.kalasoo@gmail.com>
2013-04-28 15:25:41 +0800

    add config file checker

--- fabfile.py
+++ fabfile.py
@@ -1,9 +1,10 @@
 from fabric.api import *
 from json import loads
+from config import *
 
 def p2p_test(t = 'nep2p', cf = 'config.json', d = False, l = False):
 	#check input
-	if t not in ('nep2p', 'bt', 'libswift'):
+	if t not in P2P_SYS:
 		print 'Wrong P2P system type'
 		return
 	try:
@@ -16,5 +17,30 @@ def p2p_test(t = 'nep2p', cf = 'config.json', d = False, l = False):
 	d = d if bool(d) else False;
 	l = l if bool(l) else False;
 
-	print t, cf, d, l 
-	print config
+	# print t, cf, d, l 
+	# print config
+
+	# parse configuration
+	if not check_config(config):
+		print 'Configuration file is invalid'
+		return
+	else:
+		print 'Configuration file is valid'
+
+def check_config(config):
+	if config['file_size'] not in FILE_SIZE:
+		print 'file_size'
+		return False
+	if config['num_init_peer'] <= 0:
+		print 'num_init_peer'
+		return False
+	if config['num_peer'] <= 0:
+		print 'num_peer'
+		return False
+	if len(config['nodes']) <= 1:
+		print 'nodes'
+		return False
+	if config['sender'] != None and config['sender'] not in config['nodes']:
+		print 'sender'
+		return False
+	return True
