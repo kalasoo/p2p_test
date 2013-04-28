b533ab03ce1f849089e25d6cb9a0a3804abbe3a0
kalasoo <ym.kalasoo@gmail.com>
2013-04-28 06:23:39 +0000

    add logic to p2p_test

--- p2p_test.py
+++ p2p_test.py
@@ -1,4 +1,20 @@
+#!/usr/bin/python
+
 from parser import *
 from json import load, loads
 
-config = load(args.configfile)
+print args
+try:
+	cfile = load(args.configfile)
+except:
+	print 'Incorrect config file'
+	exit()
+
+# read configuration file
+
+# write sender_config.json
+
+# write peer_config.json
+
+
+
