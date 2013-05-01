0a271de6377e85d06a88ab32c20618d8d0f04de7
kalasoo <ym.kalasoo@gmail.com>
2013-05-01 16:50:16 +0800

    add config.json.test

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ from json import loads
 P2P_SYS = ('nep2p', 'bt', 'libswft')
 FILE_SIZE = ('1K', '10K', '100K', '1M', '10M')
 DEFAULT_SENDER = 'p2plab'
+DEFAULT_PATH_BASE = '~/fyp/fyp_test/'
 
 resolver = {
 	"p2plab": {
