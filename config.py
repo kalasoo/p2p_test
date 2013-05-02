e7c14ff3dc7f73151c11dbb4bfd5e727b414583d
kalasoo <ym.kalasoo@gmail.com>
2013-05-02 19:29:07 +0800

    status

--- config.py
+++ config.py
@@ -1,11 +1,12 @@
 from json import loads
+from time import time
 
 HOST_OPTIONS = ('setup', 'start', 'getlog')
 P2P_SYS = ('nep2p', 'bt', 'libswft')
 FILE_SIZE = ('1K', '10K', '100K', '1M', '10M')
 DEFAULT_SENDER = 'p2plab'
 CONFIG_PATH_BASE = '~/fyp/fyp_nep2p/'
-LOG_PATH_BASE = '~/fyp/fyp_nep2p/log/'
+LOG_PATH_BASE = 'log/'
 
 resolver = {
 	"p2plab": {
