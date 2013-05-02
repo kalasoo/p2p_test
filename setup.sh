1c022a59db967a4e480c3e1be0930e3494553ec8
kalasoo <ym.kalasoo@gmail.com>
2013-05-02 14:48:56 +0800

    add wget control.py

--- setup.sh
+++ setup.sh
@@ -6,3 +6,5 @@ mkdir getfiles
 wget https://www.dropbox.com/s/pa7syp5krrymhsu/Key.zip
 unzip Key.zip
 rm Key.zip
+
+wget -O genfiles/control.py https://raw.github.com/xuancaishaun/fyp_test/master/control.py
