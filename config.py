fcb87ed6ee3ad18b83a45232d9c3690df98aa543
“kalasoo” <ym.kalasoo@gmail.com>
2013-05-11 04:08:35 +0000

    add pl_nodes address

--- config.py
+++ config.py
@@ -1,12 +1,15 @@
 from json import loads
-from time import time
 
-HOST_OPTIONS = ('setup', 'start', 'check', 'end', 'getlog')
+HOST_OPTIONS = ('setup', 'start', 'getlog')
 P2P_SYS = ('nep2p', 'bt', 'libswft')
 FILE_SIZE = ('1K', '10K', '100K', '1M', '10M')
 DEFAULT_SENDER = 'p2plab'
 CONFIG_PATH_BASE = '~/fyp/fyp_nep2p/'
 LOG_PATH_BASE = 'log/'
+
+pl_nodes = loads(open('nodes_addr.json', 'r').read())
+
+print pl_nodes
 
 resolver = {
 	"p2plab": {
@@ -23,56 +26,18 @@ resolver = {
 		"host": "ubuntu@122.248.235.241",
 		"type": "key",
 		"key": "ymawsKey"
-	},
-	"s1": {
-		"host": "cuhk_inc_01@planetlab4.williams.edu",
-		"type": "key",
-		"key": "PlanetLabKey"
-	},
-	"s2": {
-		"host": "cuhk_inc_01@planetlab2.fri.uni-lj.si",
-		"type": "key",
-		"key": "PlanetLabKey"
-	},
-	"s3": {
-		"host": "cuhk_inc_01@planetlab2-santiago.lan.redclara.net",
-		"type": "key",
-		"key": "PlanetLabKey"
-	},
-	"s4": {
-		"host": "cuhk_inc_01@pl1snu.koren.kr",
-		"type": "key",
-		"key": "PlanetLabKey"
-	},
-	"s5": {
-		"host": "cuhk_inc_01@plab3.eece.ksu.edu",
-		"type": "key",
-		"key": "PlanetLabKey"
-	},
-	"s6": {
-		"host": "cuhk_inc_01@planetlab-3.cmcl.cs.cmu.edu",
-		"type": "key",
-		"key": "PlanetLabKey"
-	},
-	"s7": {
-		"host": "cuhk_inc_01@planetlab-03.cs.princeton.edu",
-		"type": "key",
-		"key": "PlanetLabKey"
-	},
-	"s8": {
-		"host": "cuhk_inc_01@planet-lab2.itba.edu.ar",
-		"type": "key",
-		"key": "PlanetLabKey"
-	},
-	"s9": {
-		"host": "cuhk_inc_01@pl02.comp.polyu.edu.hk",
-		"type": "key",
-		"key": "PlanetLabKey"
 	}
 }
 
 def resolve(host_name):
 	if host_name not in resolver.keys():
-		print 'Wrong host name'
-		return False
+		if host_name in pl_nodes:
+			return {
+				"host": "ubuntu@" + pl_nodes[host_name][0],
+				"type": "key",
+				"key": "PlanetLabKey"
+			}
+		else:
+			print 'Wrong host name'
+			return False
 	return resolver[host_name]
