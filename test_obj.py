416cfc380f69f12a1a7c9e834601e6e3da8d5a4a
kalasoo <ym.kalasoo@gmail.com>
2013-05-15 13:01:46 +0800

    new test_ojb

--- test_obj.py
+++ test_obj.py
@@ -6,3 +6,13 @@ class TestObj(Object):
 	def start(self):
 		pass
 	def check(self):
+		pass
+	def end(self):
+		pass
+	def clean(self):
+		pass
+	def getlog(self):
+
+class NeP2P_TestObj(TestObj):
+	def __init__(self,config):
+		TestObj.__init__(self,config)
