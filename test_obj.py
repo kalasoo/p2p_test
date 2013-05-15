3c59f40426607e89a5efbaf1b0b6d01fa1a7ac5d
kalasoo <ym.kalasoo@gmail.com>
2013-05-15 13:17:04 +0800

    sdfsad

--- test_obj.py
+++ test_obj.py
@@ -9,10 +9,4 @@ class TestObj(Object):
 		pass
 	def end(self):
 		pass
-	def clean(self):
-		pass
-	def getlog(self):
-
-class NeP2P_TestObj(TestObj):
-	def __init__(self,config):
-		TestObj.__init__(self,config)
+		
